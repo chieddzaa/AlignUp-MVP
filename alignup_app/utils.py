9e90887e71d4cc5cc956d0d1da300009b22601ac
Natasha Matare <chiedzanatasha26@gmail.com>
2025-10-21 12:43:40 -0400

    Update utils.py

---
+++ alignup_app/utils.py
@@ -0,0 +1,10 @@
+import random
+
+def wellness_check():
+    checks = [
+        "Take a deep breath 🌿",
+        "Stretch for 2 minutes 💪",
+        "Drink some water 💧",
+        "Step away from your screen 👀",
+    ]
+    return random.choice(checks)
